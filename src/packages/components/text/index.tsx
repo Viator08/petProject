8ade6f8eec659f9da58dade5fd5f61e349aaf022
Viator08 <aarnautov08@gmail.com>
2023-01-04 13:36:15 +0300

    some ads

---
+++ src/packages/components/text/index.tsx
@@ -0,0 +1,4 @@
+import { text } from './typings';
+const text = (props: text) => (
+	<div color={`${props.color}`}>{`${props.value}`}</div>
+);
---
+++ src/packages/components/text/typings.ts
@@ -0,0 +1,9 @@
+export type text = {
+	value: string;
+	size: textSize;
+	color: textColor;
+};
+
+export type textSize = {};
+
+export type textColor = {};
